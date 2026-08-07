9321bb82e583b2afcb440b7dfa6482eb11e3fdc2
laotouhuan <919431393@qq.com>
2026-08-07 20:17:26 +0800

    Todo 1.7.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.todo.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "1.3.1"
+        versionCode = 23
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
